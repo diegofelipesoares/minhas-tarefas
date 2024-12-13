fe39cce7d237403efc78771aa956738c018ebd77
“Diego <diegofelipesoares@gmail.com>
2024-12-12 23:16:10 -0300

    Arquivo com ultimas configuração e limpo

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
 import App from './App'
 //import reportWebVitals from './reportWebVitals'
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render()
-//  <React.StrictMode>
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
-//  </React.StrictMode>,
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-//reportWebVitals()
+// Se quiser começar a medir o desempenho no seu app, passe uma função
+// para registrar os resultados (por exemplo: reportWebVitals(console.log))
+// ou envie para um endpoint de análise. Para mais informações, acesse:
+// https://bit.ly/CRA-vitals
+// reportWebVitals();
