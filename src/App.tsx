a058d4476d3a98f8d145dc4f8f0d897a90eb4da9
“Diego <diegofelipesoares@gmail.com>
2024-12-12 23:35:06 -0300

    Frase de boas vindas arrumada

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ function App() {
   return (
     <div className="App">
       <h1>Bem vindo ao React com TypeScript</h1>
+      <h3>ESlint, Prettier, Editorconfig e Styled-components</h3>
     </div>
   )
 }
