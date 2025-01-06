29fc9021559721f3d7a85f1853ca7cbd87fc60c7
“Diego <diegofelipesoares@gmail.com>
2025-01-06 19:49:34 -0300

    Navegue entre rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,32 @@
-import EstiloGlobal, { Container } from "./styles"
+import { Provider } from "react-redux"
+import { createBrowserRouter, RouterProvider} from  'react-router-dom'
+
 import BarraLateral from "./containers/BarraLateral"
 import ListaDeTarefas from "./containers/ListaDeTarefas"
+import EstiloGlobal, { Container } from "./styles"
+
 import store from './store'
-import { Provider } from "react-redux"
+import Home from "./pages/Home"
+import Cadastro from "./pages/Cadastro"
+
+const rotas = createBrowserRouter([
+  {
+    //caminho inicial - Home
+    path: '/',
+    element: <Home />
+  },
+  {
+    path: '/novo',
+    element: <Cadastro />
+  }
+])
 
 function App() {
   return (
     <Provider store={store}>
       <EstiloGlobal />
       <Container>
-        <BarraLateral />
-        <ListaDeTarefas />
+        <RouterProvider router={rotas} />
       </Container>
     </Provider>
   )
